2d84a67ddaf27d8b259e2ba09f63839cdf637001
Stewart Macias <macias.stewar@gmail.com>
2022-02-16 12:16:10 -0500

    pokeapi integration

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { getPokemons } from '../../api/getPokemons';
 import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
 function Home() {
+  
+  useEffect(() => {
+      getPokemons().then((res) => console.log(res));
+  })
+
   return (
     <div className='Home'>
       <Searcher />
